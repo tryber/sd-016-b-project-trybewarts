e6d9d6d7504c11948cbc73aba30b63e9bafd6680
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2021-09-23 02:21:33 -0300

    Todos os requisitos

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const agreeCheck = document.getElementById('agreement');
 const buttonContainer = document.querySelector('.button-container');
 const createSubmit = document.createElement('button');
 const textAreaInput = document.getElementById('textarea');
+const form1 = document.querySelector('#evaluation-form');
+const divRespostas = document.getElementById('respostas-fomulario');
+divRespostas.style.display = 'none';
 
 // Evento para verificar email e senha.
 loginButton.addEventListener('click', (event) => {
@@ -42,3 +45,85 @@ function textAreaCounter() {
 
 // Evento para contar os caracteres em tempo real
 textAreaInput.addEventListener('input', textAreaCounter);
+
+// Função para salvar os inputs - Observação
+function getTextareaInput() {
+  const textAreaValue = textAreaInput.value;
+  const p = document.getElementById('resposta-observacao');
+  p.innerText = `Observações: ${textAreaValue}`;
+}
+
+// Função para salvar os inputs - Avaliação
+function getAvaliationInput() {
+  const inputAvaliation = document.querySelectorAll('.rate-input');
+  for (let index = 0; index < inputAvaliation.length; index += 1) {
+    if (inputAvaliation[index].checked) {
+      const p = document.getElementById('resposta-avaliacao');
+      p.innerText = `Avaliação: ${inputAvaliation[index].value}`;
+    }
+  }
+  getTextareaInput();
+}
+
+// Função para salvar os inputs - Matérias
+function getConteudoInput() {
+  const inputConteudo = document.querySelectorAll('.subject');
+  const p = document.getElementById('resposta-materia');
+  const materias = [];
+  for (let index = 0; index < inputConteudo.length; index += 1) {
+    if (inputConteudo[index].checked) {
+      materias.push(` ${inputConteudo[index].value}`);
+    }
+  }
+  p.innerText = `Matérias: ${materias}`;
+  getAvaliationInput();
+}
+
+// Função para salvar os inputs - Familia
+function getFamiliaInput() {
+  const inputFamily = document.querySelectorAll('.family-check');
+  for (let index = 0; index < inputFamily.length; index += 1) {
+    if (inputFamily[index].checked) {
+      const p = document.getElementById('resposta-familia');
+      p.innerText = `Família: ${inputFamily[index].value}`;
+    }
+  }
+  getConteudoInput();
+}
+
+// Função para salvar os inputs - Casa
+function getCasaInput() {
+  const inputCasa = document.getElementById('house').value;
+  const p = document.getElementById('resposta-casa');
+  p.innerText = `Casa: ${inputCasa}`;
+  getFamiliaInput();
+}
+
+// Função para salvar os inputs - Email
+function getEmailInput() {
+  const inputEmail = document.getElementById('input-email').value;
+  const p = document.getElementById('resposta-email');
+  if (inputEmail !== '') {
+    p.innerText = `Email: ${inputEmail}`;
+  }
+  getCasaInput();
+}
+
+// Função para salvar os inputs - Nome
+function getNameInput() {
+  const inputName = document.getElementById('input-name').value;
+  const inputLastName = document.getElementById('input-lastname').value;
+  const p = document.getElementById('resposta-nome');
+  if (inputName !== '' && inputLastName !== '') {
+    p.innerText = `Nome: ${inputName} ${inputLastName}`;
+  }
+  getEmailInput();
+}
+
+// Função para apagar o formulário
+createSubmit.addEventListener('click', (event) => {
+  event.preventDefault();
+  form1.style.display = 'none';
+  divRespostas.style.display = '';
+  getNameInput();
+});
