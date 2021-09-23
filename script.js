fb4cb2d7da4434e3fa6b56fa2a798f6af8cba5c9
brunocsrribeiro <ribeiro.brunocsr@gmail.com>
2021-09-23 13:37:53 -0300

    Correcao lint

--- script.js
+++ script.js
@@ -17,21 +17,25 @@ function newChild(type, text, classs, id) {
   return newChildItem;
 }
 
-// Funcao para criar as divs containers dos input tipo radio
-function createFamilySection() {
-  const familysArray = ['Frontend', 'Backend', 'FullStack'];
-  const positionsId = ['firstFamily', 'secondFamily', 'thirdFamily'];
-  for (let index = 0; index < familysArray.length; index += 1) {
-    let newDiv = document.createElement('div');
-    newDiv.append(createInput(positionsId[index], familysArray[index]));
-    newDiv.append(createLabel(positionsId[index], familysArray[index]));
-    familyDiv.append(newDiv);
-  }
+function createCheckbox(learningArray) {
+  const newCheck = document.createElement('input');
+  newCheck.type = 'checkbox';
+  newCheck.id = `learning${learningArray}`;
+  newCheck.value = learningArray;
+  return newCheck;
+}
+
+// <label for="learningsHofs">Hofs</label>
+function createLabelCheck(learningArray) {
+  const newLabel = document.createElement('label');
+  newLabel.for = `learning${learningArray}`;
+  newLabel.innerText = learningArray;
+  return newLabel;
 }
 
 // Funcao para criar os inputs do tipo radio
 function createInput(positionId, familyArray) {
-  let newInput = document.createElement('input');
+  const newInput = document.createElement('input');
   newInput.id = positionId;
   newInput.type = 'radio';
   newInput.name = 'family';
@@ -41,7 +45,7 @@ function createInput(positionId, familyArray) {
 
 // Funcao para criar os labels dos input tipo radio
 function createLabel(positionId, familyArray) {
-  let newLabel = newChild('label', ` Familia ${familyArray}`, '', '');
+  const newLabel = newChild('label', ` Familia ${familyArray}`, '', '');
   newLabel.for = positionId;
   return newLabel;
 }
@@ -49,27 +53,23 @@ function createLabel(positionId, familyArray) {
 function createLearningSection() {
   const learningsArray = ['HoFs', 'Jest', 'Promises', 'React', 'SQL', 'Python'];
   for (let index = 0; index < learningsArray.length; index += 1) {
-    let newDiv = document.createElement('div');
+    const newDiv = document.createElement('div');
     learningDiv.append(newDiv);
     newDiv.append(createCheckbox(learningsArray[index]));
-    newDiv.append(createLabelCheck(learningsArray[index]))
+    newDiv.append(createLabelCheck(learningsArray[index]));
   }
 }
 
-function createCheckbox(learningArray) {
-  let newCheck = document.createElement('input');
-  newCheck.type = 'checkbox';
-  newCheck.id = `learning${learningArray}`;
-  newCheck.value = learningArray;
-  return newCheck;
-}
-
-// <label for="learningsHofs">Hofs</label>
-function createLabelCheck(learningArray) {
-  let newLabel = document.createElement('label');
-  newLabel.for = `learning${learningArray}`;
-  newLabel.innerText = learningArray;
-  return newLabel;
+// Funcao para criar as divs containers dos input tipo radio
+function createFamilySection() {
+  const familysArray = ['Frontend', 'Backend', 'FullStack'];
+  const positionsId = ['firstFamily', 'secondFamily', 'thirdFamily'];
+  for (let index = 0; index < familysArray.length; index += 1) {
+    const newDiv = document.createElement('div');
+    newDiv.append(createInput(positionsId[index], familysArray[index]));
+    newDiv.append(createLabel(positionsId[index], familysArray[index]));
+    familyDiv.append(newDiv);
+  }
 }
 
 // Evento do botao para verificar cadastro
@@ -88,4 +88,4 @@ loginButton.addEventListener('click', (event) => {
 window.onload = () => {
   createFamilySection();
   createLearningSection();
-}
+};
