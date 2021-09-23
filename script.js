927bf7bf07210cb01f79b0123c451bf0bdd3153b
gabrielbins <gabriel.the.bins@gmail.com>
2021-09-23 10:55:21 -0400

    requisito 18 - completo e com lint ok

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const loginButton = document.getElementById('login-button');
+const agreeBox = document.getElementById('agreement');
+const sendButton = document.getElementById('submit-btn');
 
 function checkLogin() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -11,3 +13,13 @@ function checkLogin() {
 }
 
 loginButton.addEventListener('click', checkLogin);
+
+function disableOrEnableSendButton() {
+  if (agreeBox.checked) {
+    sendButton.removeAttribute('disabled');
+  } else {
+    sendButton.setAttribute('disabled', true);
+  }
+}
+
+agreeBox.addEventListener('click', disableOrEnableSendButton);
