3eba2cc6cebf9905133b37e81f7d5f70ce030c62
Flavio Alves <flavio.alves84@gmail.com>
2021-09-23 11:10:24 -0300

    Requisitos 5-11

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Por alguma razão que ainda desconheço, ao salvar o VS Code está substituindo ' por ", fazendo com que o ESLint acuse erros. Caso ocorra, salvar com as teclas de atalho "CTRL" + "K" seguido de "S". Extensões ativas: Bracket Pair Colorizer 2; Code Runner; CSS Flexbox Cheatsheet; Dracula Official; ESLint; Material Icon Theme; Portuguese (Brazil) Language Pack for Visual Studio Code; Prettier: Code formatter; Tabnine - Code Faster with the All-Language AI Assistant for Code Completion, autocomplete JavaScript, Python, TypeScript, PHP, Go, Java, node.js, Ruby, C/C++, HTML/CSS, C#, Rust, SQL, Bash, Kotlin, R.
+
 const loginButton = document.querySelector('button');
 
 function validateEmail(event) {
