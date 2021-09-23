cb19f461745576b486b2ce68bbe495bdce9b58f5
gabrielbins <gabriel.the.bins@gmail.com>
2021-09-23 08:59:46 -0400

    resolvido o lint do js até requisito 14

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let email = document.getElementById('email');
-let password = document.getElementById('password');
-let loginButton = document.getElementById('login-button');
-
-loginButton.addEventListener('click', checkLogin);
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const loginButton = document.getElementById('login-button');
 
 function checkLogin() {
-    if (email.value == 'tryber@teste.com' && password.value == '123456'){
-        alert("Olá, Tryber!")
-    } {
-        alert("Email ou senha inválidos.")
-    }
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
 }
+
+loginButton.addEventListener('click', checkLogin);
