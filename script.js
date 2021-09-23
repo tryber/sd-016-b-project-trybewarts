116c35bafbf5abda93817b2c62a2fab11f1a92fb
João Victor Arouca <joao.silveira@estudante.ufla.br>
2021-09-23 15:23:14 -0300

    finalizado requisitos 17 e 18

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const botaoLogin = document.getElementsByClassName('botao-login')[0];
 const inputEmail = document.getElementById('input-email');
 const inputPassword = document.getElementById('input-password');
+const botaoSubmit = document.getElementById('submit-btn');
+const agreed = document.getElementById('agreement');
 
 function checkLogin() {
   if (inputEmail.value === 'tryber@teste.com' && inputPassword.value === '123456') {
@@ -10,4 +12,13 @@ function checkLogin() {
   }
 }
 
+function disable() {
+  if (agreed.checked) {
+    botaoSubmit.disabled = false;
+  } else {
+    botaoSubmit.disabled = true;
+  }
+}
+
+agreed.addEventListener('change', disable);
 botaoLogin.addEventListener('click', checkLogin);
