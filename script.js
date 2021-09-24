fab7ea2c38d7603db8d4470e7cdd6e3fc7f3d95a
Bruno Melo <melobrunoti@gmail.com>
2021-09-24 11:48:58 -0300

    Requisitos 20- 21

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputEmail = document.querySelector('.trybewarts-login #email-input');
 const inputPassword = document.querySelector('.trybewarts-login #input-password');
 const loginBtn = document.querySelector('.trybewarts-login button');
+const submitBtn = document.querySelector('#submit-btn');
 
 loginBtn.addEventListener('click', () => {
   const email = inputEmail.value;
@@ -13,9 +14,17 @@ loginBtn.addEventListener('click', () => {
   }
 });
 
-function submitForm() {
+// creditos a Andrew Hubbs pela logica abaixo, link https://stackoverflow.com/questions/14086398/count-textarea-characters
+const count = document.getElementById('counter');
+count.innerHTML = 500;
+const textareaElement = document.getElementById('textarea');
+
+textareaElement.onkeyup = function () {
+  count.innerHTML = `500/ ${500 - textareaElement.value.length}`;
+};
+
+function loginValidation() {
   const checkBox = document.querySelector('#agreement');
-  const submitBtn = document.querySelector('#submit-btn');
 
   submitBtn.disabled = true;
   checkBox.addEventListener('click', () => {
@@ -27,4 +36,37 @@ function submitForm() {
   });
 }
 
-submitForm();
+loginValidation();
+
+function materyCheck() {
+  const mats = document.getElementsByClassName('subject');
+  const matsChecked = [];
+
+  for (let i = 0; i < mats.length; i += 1) {
+    if (mats[i].checked) {
+      matsChecked.push(mats[i].value);
+    }
+  }
+  return matsChecked.join(', ');
+}
+
+function submitForm(e) {
+  e.preventDefault();
+  const nameElement = document.querySelector('#input-name').value;
+  const lastnameElement = document.querySelector('#input-lastname').value;
+  const emailElement = document.querySelector('#input-email').value;
+  const houseElement = document.querySelector('#house').value;
+  const familyElement = document.querySelector('input[name="family"]:checked').value;
+  const rateElement = document.querySelector('input[name="rate"]:checked').value;
+  const commentElement = document.querySelector('textarea').value;
+  const formContent = document.querySelector('#formContent');
+  formContent.innerText = `Nome: ${nameElement} ${lastnameElement}
+  Email: ${emailElement}
+  Casa: ${houseElement} 
+  Família: ${familyElement}
+  Matérias: ${materyCheck()}
+  Avaliação: ${rateElement}
+  Observações: ${commentElement}`;
+}
+
+submitBtn.addEventListener('click', submitForm);
