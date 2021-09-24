1addd7ab60163dbe367102584503492a78a73ea9
lucas <araujo2012@hotmail.com>
2021-09-23 21:32:26 -0300

    Requisito 14 completo

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const loginPassword = document.querySelector('.trybewarts-password');
 const loginButton = document.querySelector('.trybewarts-submit');
 const familyDiv = document.querySelector('.family-container');
 const learningDiv = document.querySelector('.learning-container');
+const ratingDiv = document.querySelector('.rating-container');
 
 // Funcao para ajudar na criacao de novos elementos
 function newChild(type, text, classs, id) {
@@ -17,6 +18,7 @@ function newChild(type, text, classs, id) {
   return newChildItem;
 }
 
+// Funcao para criar os inputs tipo checkbox
 function createCheckbox(learningArray) {
   const newCheck = document.createElement('input');
   newCheck.type = 'checkbox';
@@ -25,15 +27,7 @@ function createCheckbox(learningArray) {
   return newCheck;
 }
 
-// <label for="learningsHofs">Hofs</label>
-function createLabelCheck(learningArray) {
-  const newLabel = document.createElement('label');
-  newLabel.for = `learning${learningArray}`;
-  newLabel.innerText = learningArray;
-  return newLabel;
-}
-
-// Funcao para criar os inputs do tipo radio
+// Funcao para criar os inputs do tipo radio da secao da familia
 function createInput(positionId, familyArray) {
   const newInput = document.createElement('input');
   newInput.id = positionId;
@@ -43,13 +37,39 @@ function createInput(positionId, familyArray) {
   return newInput;
 }
 
-// Funcao para criar os labels dos input tipo radio
+// Funcao para criar os inputs do tipo radio da secao do rating
+const createInputRating = (number) => {
+  const newInput = document.createElement('input');
+  newInput.name = 'rate';
+  newInput.value = number;
+  newInput.id = `radio_${number}`;
+  newInput.type = 'radio';
+  return newInput;
+};
+
+// Funcao para criar os labels dos input tipo checkbox
+function createLabelCheck(learningArray) {
+  const newLabel = document.createElement('label');
+  newLabel.for = `learning${learningArray}`;
+  newLabel.innerText = learningArray;
+  return newLabel;
+}
+
+// Funcao para criar os labels dos input tipo radio da secao familia
 function createLabel(positionId, familyArray) {
   const newLabel = newChild('label', ` Familia ${familyArray}`, '', '');
   newLabel.for = positionId;
   return newLabel;
 }
 
+// Funcao para criar os labels dos input tipo radio da secao rating
+const createLabelRating = (number) => {
+  const newLabel = newChild('label', ` ${number}`, '', '');
+  newLabel.for = `radio_${number}`;
+  return newLabel;
+};
+
+// Funcao para criar as divs containers dos input checkbox
 function createLearningSection() {
   const learningsArray = ['HoFs', 'Jest', 'Promises', 'React', 'SQL', 'Python'];
   for (let index = 0; index < learningsArray.length; index += 1) {
@@ -60,7 +80,7 @@ function createLearningSection() {
   }
 }
 
-// Funcao para criar as divs containers dos input tipo radio
+// Funcao para criar as divs containers dos input tipo radio da secao familia
 function createFamilySection() {
   const familysArray = ['Frontend', 'Backend', 'FullStack'];
   const positionsId = ['firstFamily', 'secondFamily', 'thirdFamily'];
@@ -71,6 +91,16 @@ function createFamilySection() {
     familyDiv.append(newDiv);
   }
 }
+
+// Funcao para criar as divs containers dos input tipo radio da secao rating
+const createRatingSection = () => {
+  for (let index = 1; index <= 10; index += 1) {
+    const newDiv = document.createElement('div');
+    newDiv.append(createInputRating(index));
+    newDiv.append(createLabelRating(index));
+    ratingDiv.append(newDiv);
+  }
+};
 
 // Evento do botao para verificar cadastro
 loginButton.addEventListener('click', (event) => {
@@ -88,4 +118,5 @@ loginButton.addEventListener('click', (event) => {
 window.onload = () => {
   createFamilySection();
   createLearningSection();
+  createRatingSection();
 };
