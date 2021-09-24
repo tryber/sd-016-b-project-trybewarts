6082b557f1de6e8e07fa7f39ef5b55a70f66bbb9
bruno Marques <blmarques.dev@gmail.com>
2021-09-24 12:19:06 -0300

    Lint Correction

--- src/scripts.js
+++ src/scripts.js
@@ -27,18 +27,6 @@ contractCheck.addEventListener('click', () => {
   }
 });
 
-
-// Requisito 20 - add contador com caracteres disponiveis
-const textArea = document.querySelector('#textarea')
-const remaininhCaracteres = document.querySelector('#counter')
-const MAX_CARACTERES = 500
-
-textArea.addEventListener('input', () => {
-  const remaining = (MAX_CARACTERES - textArea.value.length)
-
-  remaininhCaracteres.textContent = `${remaining} caracteres disponiveis `
-})
-
 const contaCaracter = query('#textarea');
 contaCaracter.addEventListener('keyup', (event) => {
   const total = event.target.maxLength - event.target.textLength;
