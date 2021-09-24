28d526c3fb4efad3acce7b613c5409cb882107dd
Lucas Bertarelli <lucas.bertarelli@gmail.com>
2021-09-24 19:14:37 -0300

    finaliza requisito 20

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const inputEmail = document.querySelector('.email');
 const inputPassword = document.querySelector('.password');
 const enterButton = document.querySelector('.enter');
+const textArea = document.getElementById('textarea');
+const label = document.getElementById('counter');
+const countText = 'Caracteres Disponiveis: ';
+const textCount = document.getElementById('textarea').maxLength;
 
 function alerts() {
   if (inputEmail.value === 'tryber@teste.com' && inputPassword.value === '123456') {
@@ -12,23 +16,22 @@ function alerts() {
 enterButton.addEventListener('click', alerts);
 
 const check = document.getElementById('agreement');
-check.addEventListener('click', function checkup () {
+function checkup() {
   if (!check.checked){
     document.getElementById('submit-btn').setAttribute('disabled');
   } else {
     document.getElementById('submit-btn').removeAttribute('disabled');
   }
-});
+};
+check.addEventListener('click', checkup)
 
 function counterText() {
-  const textArea = document.getElementById('campoTexto');
-  const textCount = document.getElementById('campoTexto').maxLength;
-  const label = document.getElementById('counter');
-  const count = 'Caracteres Disponiveis: ';
-  label.innerHTML = count + textCount;
-
-  textArea.addEventListener('keypress', function reduceNumber () {
-    label.innerHTML = count + (textCount - textArea.value.length);
-  });
+  label.innerHTML = countText + textCount;
 }
 counterText()
+
+  function reduceNumber() {
+    let contador = 500;
+    label.innerHTML = (contador - textArea.value.length);
+  };
+textArea.addEventListener('keyup', reduceNumber);
