c51a75697e6ac60648a77271dad4876b0fe18aa3
Izabela <izabelahammer@gmail.com>
2021-09-24 00:49:36 -0300

    requisito 20 ok local test

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function login() {
 }
 
 document.getElementById('btn-login').addEventListener('click', login);
-// nao estou entendendo oque tem de errado na logica abaixo... requisito 18
+// requisito 18
 const btnSubmit = document.getElementById('submit-btn');
 const agree = document.getElementById('agreement');
 btnSubmit.disabled = true;
@@ -19,48 +19,13 @@ function CheckAgreement() {
   }
 }
 agree.addEventListener('change', CheckAgreement);
+// requisito 20 
+const qtChart = document.getElementById('textarea');
+const count = document.getElementById('counter');
+function countChar () {
+  count.innerText = qtChart.maxLength - qtChart.value.length;
+  return count.innerText
+}
+qtChart.addEventListener('input', countChar);
 
-// tentei de outras formas como:
-/*
-function CheckAgreement() {
-  if (btnSubmit.disabled === false) {
-    btnSubmit.disabled = true;
-  } else if (btnSubmit.disabled === true) {
-    btnSubmit.disabled = false;
-  }
-}
-agree.addEventListener('Click', CheckAgreement);
-*/
-/*
-btnSubmit.disabled = true;  botao começa desabilitado
-function CheckAgreement() {
-  if (agree.value === false) {   se o valor do agree for falso o botao estara desabilitado
-    btnSubmit.disabled = true;
-  } else if (agree.value === true) {   se o valor do agree for verdadeiro o botao estara habilitado
-    btnSubmit.disabled = false;
-  }
-}
-agree.addEventListener('Click', CheckAgreement)  quando input agree for clicado a funçao sera chamada...
- */
-// outra forma de pensar:
-/*
-function CheckAgreement() {
-  if (agree.value === false) {   se o valor do agree for falso o botao estara desabilitado
-    btnSubmit.disabled = true;
-  } else if (agree.value === true) {   se o valor do agree for verdadeiro o botao estara habilitado
-    btnSubmit.disabled = false;
-  }
-}
-btnSubmit.addEventListener('Click', CheckAgreement);  quando o enviar for clicado a função sera chamada...
- */
-// mais uma opção:  descobrir os erros do raciocinio...
-/*
-function CheckAgreement() {
-  if (agree.value === false) {   se o valor do agree for falso o botao estara desabilitado
-    btnSubmit.disabled = true;
-  }
-btnSubmit.disabled = false;
-}
-agree.addEventListener('Click', CheckAgreement)
-fontes: https://cursos.alura.com.br/forum/topico-habitar-desabilitar-botao-65202
-https://stackoverflow.com/questions/46917270/javascript-disable-button-until-all-fields-are-filled */
+
