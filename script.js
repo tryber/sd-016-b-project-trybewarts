70dc538ad0553e24d44ae1513a764a97a75fdf75
Mariana Saraiva <marianascm8@gmail.com>
2021-09-25 13:58:57 -0300

    Requisito 21

--- script.js
+++ script.js
@@ -32,3 +32,38 @@ coment.addEventListener('keyup', () => {
   const char = coment.value.length;
   cont.innerHTML = `${char}/${500 - char}`;
 });
+
+// Requisito 21
+const btnEnviar = document.getElementById('submit-btn');
+const values = [];
+function inputChecked() {
+  const materias = document.querySelectorAll('input[name="aprender"]:checked');
+  // https://pt.stackoverflow.com/questions/83463/pegar-valores-checkbox-com-javascript
+  for (let i = 0; i < materias.length; i += 1) {
+    values.push(` ${materias[i].value}`);
+  }
+  return values;
+}
+
+const novoForms = document.querySelector('#evaluation-form');
+const novoCampo = document.createElement('p');
+btnEnviar.addEventListener('click', () => {
+  const inputObjeto = {
+    studentName: document.getElementById('input-name').value,
+    lastName: document.getElementById('input-lastname').value,
+    adress: document.getElementById('input-email').value,
+    house: document.getElementById('house').value,
+    familySelected: document.querySelector('input[name="family"]:checked').value,
+    programing: inputChecked(),
+    rating: document.querySelector('input[name="rate"]:checked').value,
+    obs: document.getElementById('textarea').value };
+  novoForms.innerHTML = ' ';
+  novoCampo.innerText = `Nome: ${inputObjeto.studentName} ${inputObjeto.lastName}
+  Email: ${inputObjeto.adress}
+  Casa: ${inputObjeto.house}
+  Família: ${inputObjeto.familySelected}
+  Matérias: ${inputObjeto.programing}
+  Avaliação: ${inputObjeto.rating}
+  Observações: ${inputObjeto.obs}`;
+  novoForms.appendChild(novoCampo);
+});
