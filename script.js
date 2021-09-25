2211bec52184e520d209e84f90b77657e8ec60a5
Serafim <guilhermeserafimbonfim@hotmail.com>
2021-09-24 22:20:16 -0300

    resolve requisito 21

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const btnSubmit = document.getElementById('btn-submit');
 const inputLogin = document.querySelectorAll('input');
 const agreement = document.getElementById('agreement');
 const sendButton = document.getElementById('submit-btn');
+const counterArea = document.getElementById('counter');
+const textArea = document.getElementsByTagName('textarea');
 
 function login() {
   if (inputLogin[0].value === 'tryber@teste.com' && inputLogin[1].value === '123456') {
@@ -26,8 +28,20 @@ function activeButton() {
   }
 }
 
+function characterCount() {
+  let count = 500;
+  for (let index = 0; index < textArea[0].textLength; index += 1) {
+    count -= 1;
+    counterArea.innerText = count;
+  }
+  if (textArea[0].textLength === 0) {
+    counterArea.innerText = 500;
+  }
+}
+
 btnSubmit.addEventListener('click', login);
 agreement.addEventListener('click', activeButton);
+textArea[0].addEventListener('keyup', characterCount);
 
 window.onload = function start() {
   disableButton();
