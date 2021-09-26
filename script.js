78ebbb193045406dec9dad337cbbd35e9060e276
Renata <renatamagno@live.com>
2021-09-25 21:03:06 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Função para volidar e-mail e senha
+
 function validarSenha(event) {
   event.preventDefault();
   const inputEmail = document.querySelector('.email').value;
@@ -11,3 +13,20 @@ function validarSenha(event) {
 }
 const getBtn = document.querySelector('.bnt');
 getBtn.addEventListener('click', validarSenha);
+
+// Função habilitar/desabiitar botão
+//Referencia utilizada para solicionar requisito: 
+// https://www.w3schools.com/jsref/prop_select_disabled.asp
+
+const buttonSubmit = document.getElementById ('submit-btn');
+const inputAgreement = document.getElementById ('agreement');
+
+function enableButton () {
+  if (inputAgreement.checked) {
+    buttonSubmit.disabled = false;
+  } else {
+    buttonSubmit.disabled = true;
+  }
+}
+
+inputAgreement.addEventListener ('click' , enableButton)
