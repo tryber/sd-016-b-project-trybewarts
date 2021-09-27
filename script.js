931d352aefc81b870e3b7ee0c0f18f385a252e2e
Izabela <izabelahammer@gmail.com>
2021-09-27 02:39:27 -0300

    requisito 21 parte

--- script.js
+++ script.js
@@ -36,8 +36,16 @@ Família: Família Escolhida
 Matérias: Matérias, Marcadas, Aqui
 Avaliação: NotaAqui
 Observações: Observações aqui */
-function replaceForm() {
-  const nome = document.getElementById('evaluation-form');
-  nome.innerHTML = '';
+/* function replaceForm() {
+  const getForm = document.getElementById('evaluation-form');
+  getForm.innerHTML = '';
+  const name = document.getElementById('input-name').value;
+  const lasName = document.getElementById('input-lastname').value;
+  const email = document.getElementById('input-email').value;
+  const famlily = document.getElementById('label-family').value;
+  const classes = document.getElementById('label-content').value;
+  const rate = document.getElementById('label-rate').value;
+  const obs = document.getElementById('textarea').value;
+  getForm.innerText = 'Nome: '+ name + ' ' + lasName; ... verificar erro. colocar os outros input...
 }
-btnSubmit.addEventListener('click', replaceForm);
+btnSubmit.addEventListener('click', replaceForm); */
