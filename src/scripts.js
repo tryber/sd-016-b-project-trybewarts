12c5c107c6c843523b58e2af618c70481789ec5a
Tadeo Salaviaw <salaviawtadeo@gmail.com>
2021-09-28 22:43:44 +0100

    changed branch

--- src/scripts.js
+++ src/scripts.js
@@ -30,13 +30,13 @@ contractCheck.addEventListener('click', () => {
 
 // Requisito 20 - add contador com caracteres disponiveis
 const textArea = document.querySelector('#textarea')
-const remaininhCaracteres = document.querySelector('#counter')
+const remainingCaracteres = document.querySelector('#counter')
 const MAX_CARACTERES = 500
 
 textArea.addEventListener('input', () => {
   const remaining = (MAX_CARACTERES - textArea.value.length)
 
-  remaininhCaracteres.textContent = `${remaining} caracteres disponiveis `
+  remainingCaracteres.textContent = `${remaining} caracteres disponiveis `
 })
 
 const contaCaracter = query('#textarea');
