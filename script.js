865eff671dbb425af73d3aa8d1b89baecc92dc32
Marcus Thadeu <marcusthadeu@outlook.com>
2021-09-28 12:10:47 -0300

    teste Requisito 18

--- script.js
+++ script.js
@@ -4,12 +4,23 @@ const inputLogin = document.getElementById('email');
 const inputPassword = document.getElementById('password');
 
 function login(event) {
-event.preventDefault();
+  event.preventDefault();
   if (inputLogin.value === 'tryber@teste.com' && inputPassword.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
-};
+}
 
 buttonEntrar.addEventListener('click', login);
+
+let submitBtn = document.getElementById('submit-btn')
+let checkbox = document.getElementById('agreement')
+
+function enable(){
+  // checkbox.checked == true {
+    // document.getElementById('submit-btn').removeAttribute("disabled");
+    submitBtn.disabled = !submitBtn.disabled
+  }
+};
+checkbox.addEventListener('click', enable);
