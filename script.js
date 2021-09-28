60f072cc26cbeeeb78f626bf268796fbcee991af
Fredi Wilkens <frediwilkens@gmail.com>
2021-09-28 14:57:03 -0300

    Mudanças no estilo, inclusão do bootstrap, correção no script(parte de login)

--- script.js
+++ script.js
@@ -8,16 +8,15 @@ const lastNameInput = document.getElementById('input-lastname');
 const emailInput = document.getElementById('input-email');
 const houseInput = document.getElementById('house');
 const form = document.getElementById('evaluation-form');
-const space = ' ';
 
 btnLogin.addEventListener('click', () => {
   const email = document.getElementById('input-login').value;
   const password = document.getElementById('input-password').value;
-
-  if (email !== 'tryber@teste.com' && password !== '123456') {
-    return alert('Email ou senha inválidos.');
+  if (email === 'tryber@teste.com' && password === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
   }
-  return alert('Olá, Tryber!');
 });
 
 agreementInput.addEventListener('click', () => {
@@ -43,25 +42,24 @@ function getLearningValue() {
     if (index === 0) {
       value.push(learning[index].value);
     } else {
-      value.push(space + learning[index].value);
+      value.push(` ${learning[index].value}`);
     }
   }
   return value;
 }
 
 function createElements() {
-  const joker = ': ';
   const elementos = ['Nome', 'Email', 'Casa', 'Família', 'Matérias', 'Avaliação', 'Observações'];
   for (let index = 0; index < elementos.length; index += 1) {
     const paragraph = document.createElement('p');
-    paragraph.innerText = elementos[index] + joker + localStorage.getItem(elementos[index]);
+    paragraph.innerText = `${elementos[index]}: ${localStorage.getItem(elementos[index])}`;
     form.appendChild(paragraph);
   }
 }
 
 window.onload = () => {
   btnSubmit.addEventListener('click', () => {
-    localStorage.setItem('Nome', nameInput.value + space + lastNameInput.value);
+    localStorage.setItem('Nome', `${nameInput.value}  ${lastNameInput.value}`);
     localStorage.setItem('Email', emailInput.value);
     localStorage.setItem('Casa', houseInput.value);
     localStorage.setItem('Família', document.querySelector('[name=family]:checked').value);
