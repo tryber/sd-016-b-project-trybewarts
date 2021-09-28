502fbdf6fc147490c179985c8c728369f907543e
lucas <araujo2012@hotmail.com>
2021-09-27 22:48:23 -0300

    Requisito 21 adicionado

--- script.js
+++ script.js
@@ -8,13 +8,18 @@ const submitButton = document.querySelector('#submit-btn');
 const checkboxAgreement = document.querySelector('#agreement');
 const counterTextarea = document.querySelector('#textarea');
 const counterLegend = document.querySelector('#counter');
+const inputName = document.querySelector('#input-name');
+const inputLastName = document.querySelector('#input-lastname');
+const inputEmail = document.querySelector('#input-email');
+const inputHouse = document.querySelector('#house');
+const evaluationForm = document.querySelector('#evaluation-form');
 const inputsClass = 'form-check-input';
 
 // Funcao para criar os inputs tipo checkbox
 function createCheckbox(learningArray) {
   const newCheck = document.createElement('input');
   newCheck.type = 'checkbox';
-  newCheck.className = inputsClass;
+  newCheck.className = `learning subject ${inputsClass}`;
   newCheck.id = `learning${learningArray}`;
   newCheck.value = learningArray;
   return newCheck;
@@ -24,7 +29,7 @@ function createCheckbox(learningArray) {
 function createInput(positionId, familyArray) {
   const newInput = document.createElement('input');
   newInput.id = positionId;
-  newInput.className = inputsClass;
+  newInput.className = `family ${inputsClass}`;
   newInput.type = 'radio';
   newInput.name = 'family';
   newInput.value = familyArray;
@@ -37,7 +42,7 @@ const createInputRating = (number) => {
   newInput.name = 'rate';
   newInput.value = number;
   newInput.id = `radio_${number}`;
-  newInput.className = inputsClass;
+  newInput.className = `rating ${inputsClass}`;
   newInput.type = 'radio';
   return newInput;
 };
@@ -84,6 +89,44 @@ const createRatingSection = () => {
   }
 };
 
+const verifyFirstChecked = (check, learning) => {
+  if (check.checked === true) {
+    return (learning === '') ? `${check.value}` : `, ${check.value}`;
+  }
+  return '';
+};
+
+const getChecked = (classChecked) => {
+  const checkList = document.querySelectorAll(classChecked);
+  let learning = '';
+  for (let index = 0; index < checkList.length; index += 1) {
+    learning += verifyFirstChecked(checkList[index], learning);
+  }
+  return learning;
+};
+
+const createFormObject = () => {
+  const formObject = {
+    name: `Nome: ${inputName.value} ${inputLastName.value}`,
+    email: `Email: ${inputEmail.value}`,
+    house: `Casa: ${inputHouse.value}`,
+    family: `Família: ${getChecked('.family')}`,
+    materias: `Matérias: ${getChecked('.learning')}`,
+    avaliacao: `Avaliação: ${getChecked('.rating')}`,
+    observacao: `Observações: ${counterTextarea.value}`,
+  };
+  return formObject;
+};
+
+const updateForm = (formObject) => {
+  const values = Object.values(formObject);
+  for (let index = 0; index < values.length; index += 1) {
+    const text = document.createElement('p');
+    text.innerText = values[index];
+    evaluationForm.append(text);
+  }
+};
+
 // Evento do botao para verificar cadastro
 loginButton.addEventListener('click', (evt) => {
   const logMail = 'tryber@teste.com';
@@ -110,6 +153,14 @@ counterTextarea.addEventListener('input', (evt) => {
 // Evento para desabilitar o button Enviar quando o checkbox não estiver selecionado
 checkboxAgreement.addEventListener('change', (evt) => {
   submitButton.disabled = !evt.target.checked;
+});
+
+// Evento do botao para enviar o formulario
+submitButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  const formObject = createFormObject();
+  evaluationForm.innerHTML = '';
+  updateForm(formObject);
 });
 
 window.onload = () => {
