ff817735cba74cd1b441f2c5c5255075dfa96763
Marcus Thadeu <marcusthadeu@outlook.com>
2021-09-29 10:38:11 -0300

    Start Bonus (incomplete)

--- script.js
+++ script.js
@@ -17,30 +17,30 @@ const submitBtn = document.getElementById('submit-btn');
 const checkbox = document.getElementById('agreement');
 
 function enable() {
-  // checkbox.checked == true {
   submitBtn.removeAttribute('disabled');
   console.log('teste');
 }
 checkbox.addEventListener('click', enable);
 
-// CODIGO NAO TERMINADO
-/* const inputName = document.getElementById('input-name');
-const inputLastName = document.getElementById('input-lastname');
-const inputEmail = document.getElementById('input-email');
-const textArea = document.getElementById('textarea');
-const checkboxInputs = document.querySelectorAll('input[type=checkbox]');
+// BONUS
+// function submitData() {
+//   const inputName = document.getElementById('input-name').value;
+//   const inputLastName = document.getElementById('input-lastname').value;
+//   const inputEmail = document.getElementById('input-email').value;
+//   const inputHouse = document.getElementById('house').value;
+//   const inputFamily = document.getElementById('input[name ="family"]:checked').value;
+//   const inputRate = document.querySelectorAll('input[name ="rate"]:checked').value;
+//   const textArea = document.getElementById('textarea').value;
+  
+//   const formFull =`
+//   <p>Nome: ${inputName} ${inputLastName}</p>
+//   <p>Email: ${inputEmail}</p>
+//   <p>Casa: ${inputHouse}</p>
+//   <p>Família: ${inputFamily}</p>
+//   <p>Avaliação: ${inputRate}</p>
+//   <p>Observações: ${textArea}</p>
+//   `
+//   document.getElementById('evaluation-form').innerHTML = formFull
+// }
 
-function submitData(e) {
-  e.preventDefault();
-  for (let i = 0; i < checkboxInputs.length; i += 1) {
-    checkboxInputs[i].classList.add('subject');
-  }
-  alert('enviado');
-  inputName.innerHTML = inputName.value;
-  inputLastName.innerHTML = inputLastName.value;
-  inputEmail.innerHTML = inputEmail.value;
-  textArea.innerHTML = textArea.value;
-}
-
-submitBtn.addEventListener('click', submitData);
- */
+// submitBtn.addEventListener('click', submitData);
