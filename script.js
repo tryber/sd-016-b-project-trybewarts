7cfa583fe490d95a1f1dda9bdf73a5c6a1e80866
Pedro Fonseca <pedrinhopedro01@gmail.com>
2021-09-29 08:26:42 -0300

    remocao de uma linha inutilizada

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
 // função para validar o "usuario"
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const loginButton = document.getElementById("login-button");
-const agreeBox = document.getElementById("agreement");
-const sendButton = document.getElementById("submit-btn");
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const loginButton = document.getElementById('login-button');
+const agreeBox = document.getElementById('agreement');
+const sendButton = document.getElementById('submit-btn');
 
 function checkLogin() {
-  if (email.value === "tryber@teste.com" && password.value === "123456") {
-    alert("Olá, Tryber!");
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
   } else {
-    alert("Email ou senha inválidos.");
+    alert('Email ou senha inválidos.');
   }
 }
 
-loginButton.addEventListener("click", checkLogin);
+loginButton.addEventListener('click', checkLogin);
 
 function disableOrEnableSendButton() {
   if (agreeBox.checked) {
-    sendButton.removeAttribute("disabled");
+    sendButton.removeAttribute('disabled');
   } else {
-    sendButton.setAttribute("disabled", true);
+    sendButton.setAttribute('disabled', true);
   }
 }
 
-agreeBox.addEventListener("click", disableOrEnableSendButton);
+agreeBox.addEventListener('click', disableOrEnableSendButton);
 
 // função para contar caracteres no textarea
-const textarea = document.querySelector("#textarea");
-const info = document.querySelector("#counter");
+const textarea = document.querySelector('#textarea');
+const info = document.querySelector('#counter');
 const limite = 500;
-textarea.addEventListener("keyup", verificar);
+textarea.addEventListener('keyup', verificar);
 
 function verificar(e) {
   const qtdcaracteres = this.value.length;
   const restantes = limite - qtdcaracteres;
   info.innerHTML = restantes;
 }
-Crie um contador de caracteres 
