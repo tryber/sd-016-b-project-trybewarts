f07b797e222d324968b64074d51a6dd24c83f958
Serafim <guilhermeserafimbonfim@hotmail.com>
2021-10-07 10:28:11 -0300

    resolve todos os requisitos

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const agreement = document.getElementById('agreement');
 const sendButton = document.getElementById('submit-btn');
 const counterArea = document.getElementById('counter');
 const textArea = document.getElementsByTagName('textarea');
+const form = document.getElementById('evaluation-form');
+const selectHouse = document.getElementById('house');
+const sendName = document.getElementById('input-name');
+const lastName = document.getElementById('input-lastname');
+const sendMail = document.getElementById('input-email');
 
 function login() {
   if (inputLogin[0].value === 'tryber@teste.com' && inputLogin[1].value === '123456') {
@@ -39,9 +44,59 @@ function characterCount() {
   }
 }
 
+const isBoxChecked = () => {
+  const boxes = document.getElementsByClassName('subject');
+  const arr = [];
+  for (let index = 0; index < boxes.length; index += 1) {
+    if (boxes[index].checked === true) {
+      arr.push(boxes[index].value);
+    }
+  }
+  return arr;
+};
+
+const isRadioChecked = () => {
+  const frontEnd = document.getElementById('frontend');
+  const backEnd = document.getElementById('backend');
+  const fullStack = document.getElementById('fullStack');
+  const family = [frontEnd, backEnd, fullStack];
+  for (let index = 0; index < family.length; index += 1) {
+    if (family[index].checked === true) {
+      return family[index].value;
+    }
+  }
+};
+
+const isScoreChecked = () => {
+  const evaluation = document.getElementsByClassName('label-rate');
+  for (let index = 1; index < evaluation[0].children.length; index += 1) {
+    if (evaluation[0].children[index].checked === true) {
+      return evaluation[0].children[index].value;
+    }
+  }
+};
+
 btnSubmit.addEventListener('click', login);
 agreement.addEventListener('click', activeButton);
 textArea[0].addEventListener('keyup', characterCount);
+sendButton.addEventListener('click', () => {
+  const newText = [{
+    Nome: `${sendName.value} ${lastName.value}`,
+    Email: sendMail.value,
+    Casa: selectHouse.value,
+    Familia: isRadioChecked(),
+    Materias: isBoxChecked(),
+    Avaliacao: isScoreChecked(),
+    Observacoes: textArea[0].value,
+  }];
+  form.innerText = `Nome: ${newText[0].Nome}
+  Email: ${newText[0].Email}
+  Casa: ${newText[0].Casa}
+  Família: ${newText[0].Familia}
+  Matérias: ${newText[0].Materias.join(', ')}
+  Avaliação: ${newText[0].Avaliacao}
+  Observações: ${newText[0].Observacoes}`;
+});
 
 window.onload = function start() {
   disableButton();
