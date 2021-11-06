43e8ba9deeabc92d6d0a3baec1b2117ded703f09
Andreyrvs <cortaesai@gmail.com>
2021-11-06 15:17:38 -0300

    Requisito-3

---
+++ script.js
@@ -0,0 +1,9 @@
+const selecionaBtnEntrar = document.querySelector('.btn-entrar');
+const selecionaInputEmail = document.getElementById('input-email');
+const selecionaInputSenha = document.getElementById('input-senha');
+
+selecionaBtnEntrar.addEventListener('click', () => {
+  if (selecionaInputEmail.value === 'tryber@teste.com' && selecionaInputSenha.value === '123456') {
+    alert('Olá, Tryber!');
+  }
+});
